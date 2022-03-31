ea85df810a8ed590c4bd6330ac2bdf3ae9807f7b
setykarimi <setykarimi80@gmail.com>
2022-03-31 19:25:42 +0430

    add async await for loading data (comment just for learning)

--- src/components/FullComment/FullComment.js
+++ src/components/FullComment/FullComment.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import  './fullComment.css'
+import './fullComment.css'
+import { toast } from 'react-toastify'
 
-const FullComment = ({setComments , commentId }) => {
+const FullComment = ({ setComments, commentId }) => {
     const [comment, setComment] = useState(null)
 
     useEffect(() => {
@@ -18,14 +19,34 @@ const FullComment = ({setComments , commentId }) => {
     // comment ?
 
 
-    const deleteHandler = () => {
-        axios
-        .delete(`http://localhost:3001/comments/${commentId}` )
-        
-        .then((res) => axios.get("http://localhost:3001/comments"))
-        .then((res) => setComments(res.data))
-        .catch((err) => console.log(err))
+    // const deleteHandler = () => {
+    //     axios
+    //         .delete(`http://localhost:3001/comments/${commentId}`)
+    //         .then((res) => axios.get("http://localhost:3001/comments"))
+    //         .then((res) => setComments(res.data))
+    //         .catch((err) => console.log(err))
+    // }
+
+
+
+    const deleteHandler = async () => {
+
+        try {
+            await axios
+                .delete(`http://localhost:3001/comments/${commentId}`)
+
+            const { data } = await axios.get("http://localhost:3001/comments")
+            setComments(data)
+            toast.success("data deleted successfully")
+            setComment(null)
+        } catch (error) {
+            console.log(error);
+        }
+
+
     }
+
+
 
     const styles = {
         color: "#444",
@@ -36,18 +57,18 @@ const FullComment = ({setComments , commentId }) => {
 
     let commentDetail = <p style={styles}>please select the Comment !</p>
 
-   
 
-    if (commentId) commentDetail = <p>Loading ...</p>;
 
-    if(comment){
+    if (commentId) commentDetail = <p style={styles}>please select the Comment !</p>;
+
+    if (comment) {
         commentDetail = (
-        <div className='fullComment'>
-            <p>name : {comment.name}</p>
-            <p>email : {comment.email}</p>
-            <p>body : {comment.body}</p>
-            <button onClick={deleteHandler}>Delete</button>
-        </div>
+            <div className='fullComment'>
+                <p>name : {comment.name}</p>
+                <p>email : {comment.email}</p>
+                <p>body : {comment.body}</p>
+                <button onClick={deleteHandler}>Delete</button>
+            </div>
         )
     }
 
