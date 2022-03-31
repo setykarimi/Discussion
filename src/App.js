f7c1d3b314a845a73a438c659423ec87bab04810
setykarimi <setykarimi80@gmail.com>
2022-03-31 19:00:57 +0430

    import toastContainer to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Discussion from './container/Discussion/Discussion';
+import { ToastContainer  } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
+    <ToastContainer />
      <Discussion />
     </div>
   );
